7fa4446d4893347e61b19a23539f16451fa2a34a
blackmagic919 <actuatorspark@gmail.com>
2017-10-17 21:57:01 -0400

    I changed it so that there will be 9 lines

--- Jonathan/python_lesson1/Triangle.py
+++ Jonathan/python_lesson1/Triangle.py
@@ -1,9 +1,9 @@
 i = 1
-while i!= 10:
+while i!= 6:
     print (" "*(9-i)+"T"* (i * 2 -1) +" "* (9-i))
     i += 1 
 print (" ")
-x = 9
+x = 5
 while x!= 0:
     print (" "*(9-x)+"T"* (x * 2 - 1) +" "* (9-x))
     x -= 1 
