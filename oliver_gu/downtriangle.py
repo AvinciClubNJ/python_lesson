acc1a29413d97372b804eb9cff69aace3753ae54
OliverGu <olivergu100@gmail.com>
2017-10-20 20:32:51 -0400

    My triangle homework

--- oliver_gu/downtriangle.py
+++ oliver_gu/downtriangle.py
@@ -4,6 +4,6 @@ L = input("How many levels?\n")
 if L < 2:
   print("Invalid number")
 else:
-  for x in range(0,L-1):
-	print(" "*(L-x-1)+"/"+" "*(2*x)+"\\")
-  print("/"+"_"*2*(L-1)+"\\")
+  print("_"*2*(L))
+  for x in range(0,L):
+	print(" "*(x)+"\\"+" "*2*(L-1-x)+"/")
