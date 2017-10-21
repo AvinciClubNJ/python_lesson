f02a6c4b5bec6a4d6612040d077438ab43e7de17
Arca Baran <arcabaran@yahoo.com>
2017-10-20 20:25:44 -0400

    triangle.py

---
+++ arca/pythontest/triangle.py
@@ -0,0 +1,17 @@
+i = 9
+s = 0
+while i <= 9:
+ print (s * " " + i * "x")
+ i -= 2
+ s += 1
+ if i == -1:
+  break
+
+i = 1
+s = 4
+while i <= 9:
+ print (s * " " + i * "x")
+ i += 2
+ s -= 1
+ if i == 11:
+  break
