05d0d49855d7f328e9ede9ae36d54b74f20febcb
OliverGu <olivergu100@gmail.com>
2017-10-28 00:28:04 -0400

    second hw

---
+++ oliver_gu/newtriangle.py
@@ -0,0 +1,29 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+userinput = raw_input("What letter?\n")
+goodimput = False
+if len(userinput) > 1:
+	print("Please imput one letter.")
+elif isinstance(userinput, str) == False:
+	print("Please imput a letter.")
+elif (userinput >= 'a' and userinput <= 'z') != True:
+	print("Please imput a letter.")
+else: 
+	goodimput = True
+
+if goodimput != True:
+	print("Try again.")
+elif goodimput == True:
+	width = input("Enter an odd base width < 20.\n")
+elif isinstance(width, int) == False:
+	print("Please imput an integer.")
+elif width < 1:
+	print("Please imput a positive integer.")
+elif width > 20:
+	print("Please imput a integer smaller than 20.")
+elif width%2 == 0:
+	print("Please imput an odd integer.")
+
+if goodimput == True:	
+	for x in range(1,width):
+		print(" "*((width+1)-x-2)+userinput*(x*2-1))
