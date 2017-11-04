4176ab215ad385ee3ea8b81aa2920587efd7152e
SuperZilla66 <michaelzhhchen@gmail.com>
2017-11-04 01:54:27 +0000

    Homework 4

---
+++ Michael/homework_4/happy_birthday.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def singHappyBirthday(name):
+    print("Happy birthday to you!")
+    print("Happy birthday to you!")
+    print("Happy birthday to %s!" % name)
+    print("Happy birthday to you!")
+
+inputName = input("Enter the name to sing the Happy Birthday song: ")
+singHappyBirthday(inputName)
+
