c7d3d19755131c853695472ce212b5440434134c
OliverGu <olivergu100@gmail.com>
2017-11-04 17:24:48 -0400

    asdf

--- oliver_gu/primenumbers2.py
+++ oliver_gu/primenumbers2.py
@@ -4,35 +4,35 @@ goodinput = False
 
 while goodinput == False:
     try:
-         minimum = input("Enter minimum value.\n")
-    except NameError:
+        minimum = input("Enter minimum value.\n") 
+        if isinstance(int(minimum), int):
+            goodinput = True
+            break
+        else:
+            goodinput = False
+            print("Please input an integer.")
+    except ValueError:
         print("Please input a number.")
         continue
-    if isinstance(minimum, int):
-        goodinput = True
-        break
-    else:
-        goodinput = False
-        print("Please input an integer.")
 
 while True:
     try: 
         maximum = input("Enter maximum value.\n")
-    except NameError:
+        if isinstance(int(maximum), int):
+            if maximum <= minimum:
+                goodinput = False
+                print("Please imput a value greater than the minimum.")
+            else:
+                goodinput = True
+                break
+        else:
+            goodinput = False
+            print("Please imput an integer.")
+    except ValueError:
         print("Please input a number.")
         continue
-    if isinstance(maximum, int):
-        if maximum <= minimum:
-            goodinput = False
-            print("Please imput a value greater than the minimum.")
-        else:
-            goodinput = True
-            break
-    else:
-        goodinput = False
-        print("Please imput an integer.")
 
-for x in range(minimum,maximum+1):
+for x in range(int(minimum), int(maximum) + 1):
 	for y in range(2,x//2+1):
 		if x%y == 0:
 			break
