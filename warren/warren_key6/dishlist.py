beccf0f60656f1389122b4a0a97f0ccc21da3148
Warren Guo <warrenguo257@gmail.com>
2017-11-12 20:37:52 -0500

    Homework 6

---
+++ warren/warren_key6/dishlist.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+meatList = [
+    "Shrimp tempura",
+    "Sweet and sour shrimp",
+    "Pecan shrimp",
+    "Broccoli beef",
+    "Baked beef ribs",
+    "Beef rib stew",
+    "Deep fried steak",
+    "Curry potato beef",
+    "Deep fried pork chop",
+    "Pork meatball napa soup",
+    "Bean curd pork ribs",
+    "Sweet and sour pork ribs",
+    "Sweet and sour chicken wing",
+    "Baked chicken wing",
+    "Yan-Ju chicken",
+    "Steamed mushroom chicken",
+    "Coca-Cola chicken",
+    "Smoked chicken"
+]
+
+vegelist = [
+    "stir fry napa",
+    "stir fry bok choy",
+    "stir fry broccoli",
+    "stir fry cauliflower",
+    "stir fry mushroom",
+    "sweet and sour sliced zucchini",
+    "sweet and sour sliced locus root",
+]
